f2cfbe4a50e40adbcaa9917c6d65592209808b19
Hector <hmanrique32@gmail.com>
2017-10-16 09:28:18 +0200

    add Inject constructor to FooterRenderer

--- app/src/main/java/com/jester/marvel/ui/charactersList/renderers/FooterRenderer.kt
+++ app/src/main/java/com/jester/marvel/ui/charactersList/renderers/FooterRenderer.kt
@@ -5,11 +5,12 @@ import android.view.View
 import android.view.ViewGroup
 import com.jester.marvel.R
 import com.pedrogomez.renderers.Renderer
+import javax.inject.Inject
 
 /**
  * Created by Héctor on 11/10/2017.
  */
-class FooterRenderer : Renderer<String>() {
+class FooterRenderer @Inject constructor(): Renderer<String>() {
 
     override fun inflate(inflater: LayoutInflater, parent: ViewGroup?): View {
         return inflater.inflate(R.layout.progress_dialog, parent, false)
